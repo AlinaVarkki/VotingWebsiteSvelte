2a3f89e6fbe7d4b7db376716c4e4a362d3c3a3b3
Alina Varkki <varkkia99@gmail.com>
2021-08-10 22:13:36 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	let name = 'Alina';
+	let beltColour = 'black';
+
+	const handleClick = () => {
+		beltColour = 'orange';
+	};
+
+	const handleInput = (e) => {
+		beltColour = e.target.value;
+	};
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>{beltColour} belt!</p>
+	<button on:click={handleClick}>update belt colour</button>
+	<input type="text" on:input={handleInput} value={beltColour}>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
